840af30aa384dc023967046d2e5148c3fca0eae8
vinicius-macedo <vinicius.silva1076@etec.sp.gov.br>
2023-03-17 14:55:16 -0300

    ajustes

---
+++ src/partials/Footer.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Image, { ImageLoaderProps } from "next/image";
+import imageLoader from "../loader";
+import { DefaultMarginSection } from "@/components/DefaultMarginSection";
+
+export default function Footer() {
+  return (
+    <footer>
+      <DefaultMarginSection
+        containerClassName="bg-greenDark"
+        className="text-white flex flex-col gap-16"
+      >
+        <Image
+          src={"./img/avenue-logo-branco.png"}
+          alt={"Logo da Avenue"}
+          width={136}
+          height={26}
+          loader={imageLoader}
+          unoptimized
+        />
+        <div className="grid grid-cols-3 gap-32">
+          <article>
+            <p>© 2023 Avenue</p>
+            <p>
+              2601 S Bayshore Drive, Suite 1100 - Miami, Florida - U.S. - 33133
+            </p>
+          </article>
+          <article>
+            <p>Telefone</p>
+            <p>
+              +1 786-220-7233
+              <br />
+              (Ligação internacional)
+            </p>
+          </article>
+          <article>
+            <p>Contato</p>
+            <p>customer@avenue.us</p>
+          </article>
+        </div>
+      </DefaultMarginSection>
+    </footer>
+  );
+}
